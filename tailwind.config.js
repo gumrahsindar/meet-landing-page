ca2e862a50ebb862c4204f4ee058dd8e77883a80
Gümrah Sindar <gsindar@gmail.com>
2023-07-08 23:56:26 +0300

    define variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,31 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      tablet: '640px',
+      laptop: '1024px',
+      desktop: '1280px',
+    },
+    fontSize: {
+      sm: ['16px', '26px'],
+      base: ['18px', '26px'],
+      lg: ['32px', '36px'],
+      xl: ['40px', '44px'],
+      '2xl': ['48px', '48px'],
+      '3xl': ['64px', '64px'],
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        redHat: ['Red Hat Display', 'sans-serif'],
+      },
+      colors: {
+        'clr-teal': '#4D96A9',
+        'clr-lightBlue': '#8FE3F9',
+        'clr-purple': '#855FB1',
+        'clr-pink': '#D9B8FF',
+        'clr-black': '#28283D',
+        'clr-gray': '#87879D',
+        'clr-white': '#FAFAFA',
       },
     },
   },
