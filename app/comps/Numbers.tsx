dfd4201e5d3b7381871a66369a9afc054b0c129d
Gümrah Sindar <gsindar@gmail.com>
2023-07-11 21:50:11 +0300

    complete number divider

---
+++ app/comps/Numbers.tsx
@@ -0,0 +1,14 @@
+const Numbers = () => {
+  return (
+    <section
+      id='numbers'
+      className='flex flex-col items-center mt-16 tablet:mt-20 laptop:mt-28 mb-20 tablet:mb-16'
+    >
+      <div className='border-l h-[84px]  border-clr-gray border-opacity-25 '></div>
+      <div className='flex items-center justify-center border rounded-full  border-clr-gray border-opacity-25  w-14 h-14'>
+        <p className='text-clr-gray font-redHat font-black text-sm'>01</p>
+      </div>
+    </section>
+  )
+}
+export default Numbers
