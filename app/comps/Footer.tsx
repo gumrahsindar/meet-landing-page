a4311ec148ce4955e40ebf249455b8df5ea7fc40
Gümrah Sindar <gsindar@gmail.com>
2023-07-12 22:37:09 +0300

    finish footer

--- app/comps/Footer.tsx
+++ app/comps/Footer.tsx
@@ -2,7 +2,10 @@ import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <footer id='footer' className='relative overflow-hidden -z-10 '>
+    <footer
+      id='footer'
+      className='relative overflow-hidden max-h-[500px] laptop:max-h-80'
+    >
       <img
         src='/assets/mobile/image-footer.jpg'
         alt='footer'
@@ -16,18 +19,18 @@ const Footer = () => {
       <img
         src='/assets/desktop/image-footer.jpg'
         alt='footer'
-        className='hidden tablet:hidden laptop:block w-full bg-no-repeat'
+        className='hidden tablet:hidden laptop:block w-full bg-no-repeat bg-cover'
       />
       <div className='absolute inset-0 bg-clr-teal opacity-90 '></div>
-      <div className='flex flex-col laptop:flex-row justify-center items-center text-center z-10]'>
-        <h4 className='absolute text-lg text-clr-white font-redHat font-black  top-20 '>
+      <div className='flex flex-col laptop:flex-row laptop:justify-evenly justify-center items-center text-center'>
+        <h4 className='absolute text-lg tablet:text-xl text-clr-white font-redHat font-black top-20 laptop:left-36 laptop:top-24 max-w-sm laptop:max-w-sm'>
           Experience more together
         </h4>
-        <p className='absolute font-redHat text-clr-white top-48 px-6 max-w-xs'>
+        <p className='absolute font-redHat text-clr-white top-52 px-6 max-w-xl laptop:max-w-xs laptop:top-28'>
           Stay connected with reliable HD meetings and unlimited one-on-one and
           group video sessions.
         </p>
-        <button className='absolute text-sm font-redHat font-bold text-white bg-clr-purple px-7 py-4 rounded-full top-80'>
+        <button className='absolute text-sm font-redHat font-bold text-white bg-clr-purple px-7 py-4 rounded-full top-80 laptop:right-56 laptop:top-28 z-30 hover:bg-[#B18BDD] duration-200 '>
           Download{' '}
           <span className='text-sm font-redHat font-black text-clr-pink'>
             v1.3
