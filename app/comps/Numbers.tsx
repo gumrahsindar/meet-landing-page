eb13a3f5191782f3d2dcd68e5c65e053626a3406
Gümrah Sindar <gsindar@gmail.com>
2023-07-12 22:37:01 +0300

    add prop

--- app/comps/Numbers.tsx
+++ app/comps/Numbers.tsx
@@ -8,10 +8,10 @@ const Numbers = ({ number, marginBottom, translateY }: NumberProps) => {
   return (
     <section
       id='numbers'
-      className={`flex flex-col items-center mt-16 tablet:mt-20 laptop:mt-28 ${marginBottom} ${translateY} `}
+      className={`relative z-10 flex flex-col items-center mt-16 tablet:mt-20 laptop:mt-28 ${marginBottom} ${translateY} `}
     >
       <div className='border-l h-[84px]  border-clr-gray border-opacity-25 '></div>
-      <div className='flex items-center justify-center border rounded-full  border-clr-gray border-opacity-25 bg-white  w-14 h-14'>
+      <div className='flex items-center justify-center border rounded-full  border-clr-gray border-opacity-25 bg-white z-20  w-14 h-14'>
         <p className='text-clr-gray font-redHat font-black text-sm'>{number}</p>
       </div>
     </section>
